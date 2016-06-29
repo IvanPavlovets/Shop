0bcf43f47a6c8f20ad8648bce3cce88d72e6fe00
IvanPavlovets <ivan150287@gmail.com>
2016-06-29 15:40:33 +0700

    filled AbstractEmployee class

--- src/com/commander/shop/staff/AbstractEmployee.java
+++ src/com/commander/shop/staff/AbstractEmployee.java
@@ -1,4 +1,54 @@
 package com.commander.shop.staff;
 
-public class AbstractEmployee {
+import com.commander.shop.interfaces.DepartmentInterface;
+import com.commander.shop.interfaces.EmployeeInterface;
+
+public class AbstractEmployee implements EmployeeInterface{
+
+    public AbstractEmployee() {
+    }
+
+    public AbstractEmployee(String name, int exp) {
+        this.name = name;
+        this.exp = exp;
+    }
+
+    private String name;
+    private int exp;
+    private DepartmentInterface department;
+    private boolean free;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getExp() {
+        return exp;
+    }
+
+    @Override
+    public DepartmentInterface getDepartment() {
+        return department;
+    }
+
+    @Override
+    public void setDepartment(DepartmentInterface department) {
+
+    }
+
+    @Override
+    public boolean isFree() {
+        return free;
+    }
+
+    @Override
+    public void getInfo() {
+
+    }
 }
